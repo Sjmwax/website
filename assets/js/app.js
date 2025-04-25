bdd28a1c3ecec8a8f2faaaf0bbe3fc594c414e0e
Sjmwax <134770323+Sjmwax@users.noreply.github.com>
2025-04-25 15:16:10 +0200

    Update app.js
    Editing footer using JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded",()=>{
     const currentYear = new Date().getFullYear();
-    const currentYearSpans = document.querySelectorAll(['data-year']);
-    currentYearSpans.forEach(element =>{
-        element.textContent = currentYear;
+
+
+
+    document.getElementById("current-year").textContent = currentYear;
+
+
     });
-});
